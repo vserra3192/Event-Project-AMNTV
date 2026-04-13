14a49a1e962060c127eb84d0fc69a501b1e407f7
timsereda <timothysered@umass.edu>
2026-04-13 01:12:29 +0000

    feat: define comment error types

--- src/repository/CommentRepository.ts
+++ src/repository/CommentRepository.ts
@@ -5,3 +5,17 @@ export interface IComment {
   content: string;
   createdAt: Date;
 }
+
+export type CommentError =
+  | { name: "CommentNotFound"; message: string }
+  | { name: "InvalidContent"; message: string };
+
+export const CommentNotFound = (message: string): CommentError => ({
+  name: "CommentNotFound",
+  message,
+});
+
+export const InvalidContent = (message: string): CommentError => ({
+  name: "InvalidContent",
+  message,
+});
