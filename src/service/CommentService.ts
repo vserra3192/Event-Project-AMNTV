28584a0c355a052dd8cd8277cabaa9d29bebc964
timsereda <timothysered@umass.edu>
2026-04-14 18:50:06 +0000

    feat: define comment service interface

--- src/service/CommentService.ts
+++ src/service/CommentService.ts
@@ -1,7 +1,15 @@
 import type { UserRole } from '../auth/User';
+import type { Result } from '../lib/result';
+import type { CommentError, IComment } from '../repository/CommentRepository';
 
 export type User = {
   userId: String;
   displayName: String;
   role: UserRole;
 };
+
+export interface ICommentService {
+  getCommentsByEventId(eventId: number): Promise<Result<IComment[], CommentServiceError>>;
+  addComment(eventId: number, content: string, actor: User): Promise<Result<IComment, CommentServiceError>>;
+  deleteComment(commentId: number, actor: User): Promise<Result<void, CommentServiceError>>;
+}
