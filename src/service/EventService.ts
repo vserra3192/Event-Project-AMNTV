ddbe8dd5e1b67aa9e6df76b4f8801c09db745cd8
mma <mmalves@umass.edu>
2026-04-14 15:23:01 -0400

    feat: added service interfaces into EventService.ts

--- src/service/EventService.ts
+++ src/service/EventService.ts
@@ -1,23 +1,24 @@
 import { Err, Ok, type Result } from "../lib/result";
-import type { IEvent, EventError, IEventRepository } from "../repository/EventRepository";
-import { InvalidID } from "../repository/EventRepository";
+import { IEvent, IEventRepository, CreateEventInput, EventStatus} from "../repository/EventRepository";
+import { type EventError, UnexpectedRepositoryError, ValidationError} from "../repository/Errors";
+
+export interface CreateEventServiceInput {
+  title: string;
+  description: string;
+  location: string;
+  category: string;
+  status: EventStatus;
+  capacity: number | null;
+  startDatetime: Date;
+  endDatetime: Date;
+}
 
 export interface IEventService {
   getEventByID(id: number): Promise<Result<IEvent, EventError>>;
   getAllEvents(): Promise<Result<IEvent[], EventError>>;
 }
 
-export class EventService implements IEventService {
-  constructor(private readonly eventRepository: IEventRepository) {}
-
-  async getEventByID(id: number): Promise<Result<IEvent, EventError>> {
-    if (!Number.isInteger(id) || id <= 0) {
-      return Err(InvalidID("ID must be a positive integer."));
-    }
-    return await this.eventRepository.getEventById(id);
-  }
-
-  async getAllEvents(): Promise<Result<IEvent[], EventError>> {
-    return await this.eventRepository.getAllEvents();
-  }
+export interface IEventService {
+  createEvent(input: CreateEventServiceInput, organizerId: string): Promise<Result<IEvent, EventError>>;
+  getAllEvents(): Promise<Result<IEvent[], EventError>>;
 }
