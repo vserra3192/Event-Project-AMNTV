6130a48868bef327f72d7e3352e8ced2ababfd80
mma <mmalves@umass.edu>
2026-04-14 15:10:47 -0400

    feat: added error handling for eventrepo

---
+++ src/repository/Errors.ts
@@ -0,0 +1,19 @@
+export type EventError =
+  | { name: 'EventNotFound'; message: string }
+  | { name: 'InvalidId'; message: string }
+  | { name: 'UnexpectedRepositoryError'; message: string };
+ 
+export const EventNotFound = (message: string): EventError => ({
+  name: 'EventNotFound',
+  message,
+});
+ 
+export const InvalidId = (message: string): EventError => ({
+  name: 'InvalidId',
+  message,
+});
+ 
+export const UnexpectedRepositoryError = (message: string): EventError => ({
+  name: 'UnexpectedRepositoryError',
+  message,
+});
