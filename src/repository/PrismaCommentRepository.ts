467676dbb4e79f51c381c0b737b463dc6d19070f
timsereda <timothysered@umass.edu>
2026-04-23 15:13:43 -0400

    feat: prisma get comments by id method

--- src/repository/PrismaCommentRepository.ts
+++ src/repository/PrismaCommentRepository.ts
@@ -5,9 +5,7 @@ import { CommentError, CommentNotFound, IComment, ICommentRepository } from "./I
 export class PrismaCommentRepository implements ICommentRepository {
   constructor(private readonly prisma: PrismaClient) {}
 
-  async getCommentsByEventId(
-    eventId: number
-  ): Promise<Result<IComment[], CommentError>> {
+  async getCommentsByEventId(eventId: number): Promise<Result<IComment[], CommentError>> {
     try {
       const comments = await this.prisma.comment.findMany({
         where: { eventId },
@@ -17,6 +15,22 @@ export class PrismaCommentRepository implements ICommentRepository {
       return Ok(comments);
     } catch {
       return Err(CommentNotFound("Failed to fetch comments."));
+    }
+  }
+
+  async getCommentById(id: number): Promise<Result<IComment, CommentError>> {
+    try {
+      const comment = await this.prisma.comment.findUnique({
+        where: { id },
+      });
+
+      if (!comment) {
+        return Err(CommentNotFound(`Comment ${id} not found.`));
+      }
+
+      return Ok(comment);
+    } catch {
+      return Err(CommentNotFound("Failed to fetch comment."));
     }
   }
 }
