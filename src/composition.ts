d12d32e3110637ff081ba790d47940a3e8f28173
Abhisheha Muthuswamy <abhishehamut@umass.edu>
2026-04-23 12:54:10 -0400

    merge conflict fix

--- src/composition.ts
+++ src/composition.ts
@@ -38,16 +38,13 @@ export function createComposedApp(logger?: ILoggingService): IApp {
   const rsvpService = new RSVPService(rsvpRepository, eventRepo);
   const rsvpController = new RSVPController(rsvpService, resolvedLogger);
 
-<<<<<<< HEAD
   const eventController = CreateController(
   eventService,
   rsvpService,
   resolvedLogger,
   adminUserService
 );
-=======
->>>>>>> 644493cdb2358db66cc51d4357149e5aed802ce4
-return CreateApp(
+  return CreateApp(
   authController,
   eventController,
   commentController,
