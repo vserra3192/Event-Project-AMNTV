171e1d7e5001beea1381867d57686feae1a24fc8
timsereda <timothysered@umass.edu>
2026-04-23 15:10:57 -0400

    feat: prisma get comments method

--- src/repository/PrismaCommentRepository.ts
+++ src/repository/PrismaCommentRepository.ts
@@ -1,5 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { Err, Ok, type Result } from "../lib/result";
+import { CommentError, CommentNotFound, IComment, ICommentRepository } from "./InMemoryCommentRepository";
 
 export class PrismaCommentRepository implements ICommentRepository {
   constructor(private readonly prisma: PrismaClient) {}
+
+  async getCommentsByEventId(
+    eventId: number
+  ): Promise<Result<IComment[], CommentError>> {
+    try {
+      const comments = await this.prisma.comment.findMany({
+        where: { eventId },
+        orderBy: { createdAt: "asc" },
+      });
+
+      return Ok(comments);
+    } catch {
+      return Err(CommentNotFound("Failed to fetch comments."));
+    }
+  }
 }
