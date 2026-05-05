0ef3548d8226ddac93a0bc65dfcc04f2ed67631e
NickSouth <nickrsouthey@gmail.com>
2026-05-05 15:02:19 -0400

    test: testing new friendship repo methods

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -70,12 +70,44 @@ beforeAll(async () => { //create test.db schema based on our dev.db schema incas
     CREATE UNIQUE INDEX IF NOT EXISTS "User_email_key"
     ON "User"("email")
   `);
+
+  await prisma.$executeRawUnsafe(`
+    CREATE TABLE IF NOT EXISTS "Friend" (
+      "userId" TEXT NOT NULL,
+      "friendId" TEXT NOT NULL,
+      "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY ("userId", "friendId"),
+      CONSTRAINT "Friend_userId_fkey"
+        FOREIGN KEY ("userId") REFERENCES "User" ("id")
+        ON DELETE CASCADE ON UPDATE CASCADE,
+      CONSTRAINT "Friend_friendId_fkey"
+        FOREIGN KEY ("friendId") REFERENCES "User" ("id")
+        ON DELETE CASCADE ON UPDATE CASCADE
+    )
+  `);
+
+  await prisma.$executeRawUnsafe(`
+    CREATE TABLE IF NOT EXISTS "FriendRequest" (
+      "requesterId" TEXT NOT NULL,
+      "recipientId" TEXT NOT NULL,
+      "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY ("requesterId", "recipientId"),
+      CONSTRAINT "FriendRequest_requesterId_fkey"
+        FOREIGN KEY ("requesterId") REFERENCES "User" ("id")
+        ON DELETE CASCADE ON UPDATE CASCADE,
+      CONSTRAINT "FriendRequest_recipientId_fkey"
+        FOREIGN KEY ("recipientId") REFERENCES "User" ("id")
+        ON DELETE CASCADE ON UPDATE CASCADE
+    )
+  `);
 });
 
 beforeEach(async () => {
   await prisma.comment.deleteMany();
   await prisma.eventRsvp.deleteMany();
   await prisma.event.deleteMany();
+  await prisma.friendRequest.deleteMany();
+  await prisma.friend.deleteMany();
 });
 
 afterAll(async () => {
